9abfeaf8392b4ff96d5899ab005b65d635944049
Tarachan Rana <103596256+Rtarachan02@users.noreply.github.com>
2024-12-29 17:01:45 +0530

    Some changes

--- Lecture Codes/tut4_somemoreWidgets.py
+++ Lecture Codes/tut4_somemoreWidgets.py
@@ -1,12 +1,34 @@
 import sys
-from PyQt6.QtWidgets import QMainWindow,QApplication,QLabel,QWidget
+from PyQt6.QtWidgets import QMainWindow,QApplication,QLabel,QWidget,QPushButton,QVBoxLayout
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
     def initUI(self):
-        self.setWindowTitle("Some more Widgets")
+        self.setWindowTitle("Some major classes")
+        # create a label
+        label=QLabel("Hello PyQt")
+
+        # create a push button
+        button=QPushButton("Click Me")
+        button.clicked.connect(self.onButtonClick)
+
+        # create a vertical layout
+        layout=QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(button)
+
+        # create a central widget and set the layout
+        central_widget=QWidget()
+        central_widget.setLayout(layout)
+
+        self.setCentralWidget(central_widget)
+
+    def onButtonClick(self):
+        print("Button Clicked..")
+
+
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
